2b0b3b0168b9d120689aeb488e64333bb38f6656
BojanVr <bojanvrskovski@gmail.com>
2023-10-21 16:54:30 +0200

    added the test for the ContactServiceImplTest class

--- catori-tech-assessment/src/main/java/org/catoritech/entity/Contact.java
+++ catori-tech-assessment/src/main/java/org/catoritech/entity/Contact.java
@@ -26,4 +26,52 @@ public class Contact {
 		this.phoneNumber = phoneNumber;
 		this.vat = vat;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getVat() {
+		return vat;
+	}
+
+	public void setVat(String vat) {
+		this.vat = vat;
+	}
 }
--- catori-tech-assessment/src/test/java/org/catoritech/service/impl/ContactServiceImplTest.java
+++ catori-tech-assessment/src/test/java/org/catoritech/service/impl/ContactServiceImplTest.java
@@ -1,5 +1,23 @@
 package org.catoritech.service.impl;
 
+import io.quarkus.test.junit.QuarkusTest;
+import org.catoritech.entity.Contact;
+import org.junit.jupiter.api.Test;
+import jakarta.inject.Inject;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
 public class ContactServiceImplTest {
 
+	@Inject
+	ContactServiceImpl contactService;
+
+
+	@Test
+	public void testReadAll() {
+		List<Contact> contacts = contactService.readAll();
+		assertEquals(contacts.size(), contacts.size());
+	}
 }
