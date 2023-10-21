293b70493ef627dd99943d21378f60df6ce38914
BojanVr <bojanvrskovski@gmail.com>
2023-10-21 16:23:32 +0200

    added test for the resource readAllContacts method

--- catori-tech-assessment/src/test/java/org/catoritech/resource/ContactResourceTest.java
+++ catori-tech-assessment/src/test/java/org/catoritech/resource/ContactResourceTest.java
@@ -1,5 +1,20 @@
 package org.catoritech.resource;
 
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
+import jakarta.ws.rs.core.HttpHeaders;
+import static io.restassured.RestAssured.given;
+@QuarkusTest
 public class ContactResourceTest {
 
+	@Test
+	public void testReadAllContacts_success(){
+		given().
+			header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).
+		when().
+			get("api/contact/all").
+		then().
+			statusCode(200);
+	}
 }
