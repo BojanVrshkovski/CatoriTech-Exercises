d402ef6c60372a8486584770ee91a2ece522d9fc
BojanVr <bojanvrskovski@gmail.com>
2023-10-22 14:32:00 +0200

    added test for the service impl class

--- catori-tech-assessment/src/test/java/org/catoritech/service/impl/ContactServiceImplTest.java
+++ catori-tech-assessment/src/test/java/org/catoritech/service/impl/ContactServiceImplTest.java
@@ -5,8 +5,8 @@ import org.catoritech.entity.Contact;
 import org.junit.jupiter.api.Test;
 import jakarta.inject.Inject;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
 public class ContactServiceImplTest {
@@ -19,5 +19,22 @@ public class ContactServiceImplTest {
 	public void testReadAll() {
 		List<Contact> contacts = contactService.readAll();
 		assertEquals(contacts.size(), contacts.size());
+	}
+
+	@Test
+	public void testReadById() {
+		Contact newContact = new Contact(1L,"John","Doe","123 Main St","555-123-4567","12345A");
+
+		Long contactId = newContact.getId();
+		Contact retrievedContact = contactService.readById(contactId);
+
+		assertNotNull(retrievedContact);
+
+		assertEquals(1L, retrievedContact.getId());
+		assertEquals("John", retrievedContact.getName());
+		assertEquals("Doe", retrievedContact.getLastName());
+		assertEquals("123 Main St", retrievedContact.getAddress());
+		assertEquals("555-123-4567", retrievedContact.getPhoneNumber());
+		assertEquals("12345A", retrievedContact.getVat());
 	}
 }
